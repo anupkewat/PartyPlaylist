adb4c0a7ad19f93563969312226a5661968bace2
anupkewat <anup.kewat@gmail.com>
2024-01-25 17:11:50 +0530

    removed acess token from test file

--- backend/test.js
+++ backend/test.js
@@ -6,8 +6,7 @@ const spotifyApi = new SpotifyWebApi({
     clientSecret: process.env.CLIENT_SECRET,
   })
 
-accessToken = 'BQCFqe2lvGD1KoHtzyL2zgK3nu03a-vNtDGWUlTZo8Mv87ruD9hZ5cgFd31AdqwTgcs-r_O-8DNXxd5MagGduNpuAUFaAwl-BH-3H0Oywd0Fg9bd9-Ibarr5917DgOhrHwMGsoNifzvy6iqUl0Y1lpE8xC3IR4V87d6jP4fjDa4j1FVoFN0Z8AV4O6S7cL1EsyOcFeLSLjMmiWWLgmd7A4dn-OTTZaD03MwllA42tLYU0-BQqv875U1wzrvsiPWica__JufRepX79UE3LHSeUw_XtXMUZQlWHzHhlMrDhSlxpME'
-
+accessToken = ''
 spotifyApi.setAccessToken(accessToken);
   console.log("Creating Playlist ...")
   
